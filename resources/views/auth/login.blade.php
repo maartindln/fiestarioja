f832c3a1e56575c83978d04cefc50f446777209a
laander47 <146874555+laander47@users.noreply.github.com>
2025-08-01 18:37:03 +0200

    alertas

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -6,21 +6,20 @@
     <script src="https://cdn.tailwindcss.com"></script>
     <link rel="icon" href="images/png/logo.png" type="image/png">
     <title>Iniciar Sesión</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css"/>
 </head>
 <body class="bg-green-950 flex flex-col justify-center min-h-screen py-12">
 
-    @if (session('error'))
-    <div class="alert alert-error d-flex align-items-center" role="alert"
-        style="position: fixed; top: 14%; left: 50%; transform: translate(-50%, -50%);
-               width: 60%; max-width: 600px; margin: 0; z-index: 1050;
-               justify-content: center; text-align: center; border-radius: 15px; padding: 10px;
-               background-color: #f8d7da; color: #721c24; display: flex; align-items: center;">
-        <svg class="bi flex-shrink-0 me-2" width="24" height="24" role="img" aria-label="Error"
-             style="margin-right: 10px;">
-            <use xlink:href="#exclamation-triangle-fill" />
-        </svg>
-        <div style="padding: 0;">
-            {{ session('error') }}
+@if (session('error'))
+    <div class="alert flex fixed inset-x-0 top-0 mx-auto mt-4 w-fit max-w-md z-50 w-96 shadow-lg rounded-lg animate__animated animate__fadeInDown">
+        <div class="bg-red-600 py-4 px-6 rounded-l-lg flex items-center">
+            <svg xmlns="http://www.w3.org/2000/svg" class="text-white fill-current" viewBox="0 0 16 16" width="20" height="20"><path fill-rule="evenodd" d="M13.78 4.22a.75.75 0 010 1.06l-7.25 7.25a.75.75 0 01-1.06 0L2.22 9.28a.75.75 0 011.06-1.06L6 10.94l6.72-6.72a.75.75 0 011.06 0z"></path></svg>
+        </div>
+        <div class="px-4 py-6 bg-white rounded-r-lg flex justify-between items-center w-full border border-l-transparent border-gray-200">
+        <div>{{ session('error') }}</div>
+            <button onclick="closeAlert(this)">
+                <i class="fa-solid fa-xmark mr-4 ml-4"></i>
+            </button>
         </div>
     </div>
 @endif
@@ -155,3 +154,23 @@
     </script>
 </body>
 </html>
+
+<script>
+    function closeAlert(button) {
+        const alert = button.closest('.alert');
+        if (alert) {
+            alert.classList.remove('animate__fadeInDown');
+            alert.classList.add('animate__fadeOutUp');
+            setTimeout(() => alert.remove(), 800);
+        }
+    }
+
+     setTimeout(() => {
+            const alert = document.querySelector('.alert');
+            if (alert) {
+                alert.classList.remove('animate__fadeInDown');
+                alert.classList.add('animate__fadeOutUp');
+                setTimeout(() => alert.remove(), 800);
+            }
+        }, 5000);
+</script>
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -8,6 +8,33 @@
     <title>Regístrate</title>
 </head>
 <body class="bg-green-950 flex flex-col justify-center min-h-screen py-10">
+    @if (session('success'))
+    <div class="alert flex fixed inset-x-0 top-0 mx-auto mt-4 w-fit max-w-md z-50 w-96 shadow-lg rounded-lg animate__animated animate__fadeInDown">
+        <div class="bg-green-600 py-4 px-6 rounded-l-lg flex items-center">
+            <i class="fa-solid fa-circle-check text-white text-2xl"></i>
+        </div>
+        <div class="px-4 py-6 bg-white rounded-r-lg flex justify-between items-center w-full border border-l-transparent border-gray-200">
+        <div>{{ session('success') }}</div>
+            <button onclick="closeAlert(this)">
+                <i class="fa-solid fa-xmark mr-4 ml-4"></i>
+            </button>
+        </div>
+    </div>
+@endif
+
+@if (session('error'))
+    <div class="alert flex fixed inset-x-0 top-0 mx-auto mt-4 w-fit max-w-md z-50 w-96 shadow-lg rounded-lg animate__animated animate__fadeInDown">
+        <div class="bg-red-600 py-4 px-6 rounded-l-lg flex items-center">
+            <svg xmlns="http://www.w3.org/2000/svg" class="text-white fill-current" viewBox="0 0 16 16" width="20" height="20"><path fill-rule="evenodd" d="M13.78 4.22a.75.75 0 010 1.06l-7.25 7.25a.75.75 0 01-1.06 0L2.22 9.28a.75.75 0 011.06-1.06L6 10.94l6.72-6.72a.75.75 0 011.06 0z"></path></svg>
+        </div>
+        <div class="px-4 py-6 bg-white rounded-r-lg flex justify-between items-center w-full border border-l-transparent border-gray-200">
+        <div>{{ session('error') }}</div>
+            <button onclick="closeAlert(this)">
+                <i class="fa-solid fa-xmark mr-4 ml-4"></i>
+            </button>
+        </div>
+    </div>
+@endif
     <div class="absolute top-6 left-6">
         <button onclick="window.history.back()" class="px-4 py-2 text-sm font-semibold text-green-950 bg-yellow-400 border-2 border-yellow-400 rounded-md shadow-md hover:bg-yellow-500 focus:outline-none">
             VOLVER
@@ -68,3 +95,23 @@
     </div>
 </body>
 </html>
+
+<script>
+    function closeAlert(button) {
+        const alert = button.closest('.alert');
+        if (alert) {
+            alert.classList.remove('animate__fadeInDown');
+            alert.classList.add('animate__fadeOutUp');
+            setTimeout(() => alert.remove(), 800);
+        }
+    }
+
+     setTimeout(() => {
+            const alert = document.querySelector('.alert');
+            if (alert) {
+                alert.classList.remove('animate__fadeInDown');
+                alert.classList.add('animate__fadeOutUp');
+                setTimeout(() => alert.remove(), 800);
+            }
+        }, 5000);
+</script>
