84050053a4a2e43d904aadf76b77a004115870f6
laander47 <146874555+laander47@users.noreply.github.com>
2026-05-02 16:22:33 +0200

    terminos

--- app/Http/Controllers/PrivController.php
+++ app/Http/Controllers/PrivController.php
@@ -11,7 +11,10 @@ class PrivController extends Controller
      */
     public function politicas()
     {
-        // Esto busca el archivo en resources/views/politicas/priv.blade.php
         return view('politicas.priv');
+    }
+    public function terminos()
+    {
+        return view('politicas.terminos');
     }
 }
